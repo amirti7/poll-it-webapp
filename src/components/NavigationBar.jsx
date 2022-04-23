c5cdabea8144e9aedbb18db580bf796d8b8af682
Amir Titelman <amirt@ndrip.com>
2022-04-23 15:50:04 +0300

    handle signed in users in nav bar

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,8 +1,44 @@
 import "bootstrap/dist/css/bootstrap.css";
 import { Nav, Navbar } from "react-bootstrap";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import PollItLogo from "../assets/images/Logo.png";
+import { useState } from "react";
 
 const NavigationBar = (props) => {
+  const user = localStorage.getItem("CheckedInUser");
+  const [userCheckedOut, setUserCheckedOut] = useState(false);
+
+  const signOut = () => {
+    localStorage.removeItem("CheckedInUser");
+    setUserCheckedOut(true);
+  };
+
+  const normal = (
+    <>
+      <Nav.Item className="ms-auto">
+        <Nav.Link href="/login">Login</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="/signup">Sign-Up</Nav.Link>
+      </Nav.Item>
+    </>
+  );
+
+  const userLoggedIn = (
+    <>
+      <Nav.Item className="ms-auto">
+        <Nav.Link href="">
+          <AccountCircleIcon />
+          {user}
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link href="" onClick={signOut}>
+          Sign Out
+        </Nav.Link>
+      </Nav.Item>
+    </>
+  );
   return (
     <Navbar
       style={{ marginBottom: "50px" }}
@@ -24,12 +60,7 @@ const NavigationBar = (props) => {
         <Nav.Item>
           <Nav.Link href="/contact_us">New Poll</Nav.Link>
         </Nav.Item>
-        <Nav.Item className="ms-auto">
-          <Nav.Link href="/login">Login</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link href="/signup">Sign-Up</Nav.Link>
-        </Nav.Item>
+        {user ? userLoggedIn : normal}
       </Nav>
     </Navbar>
   );
