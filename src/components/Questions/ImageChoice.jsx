738696cd3f1024d53dfb6594aa797131e6f7786d
Amir Titelman <amirt@ndrip.com>
2022-04-28 01:40:05 +0300

    post a poll to the server implementation init

--- src/components/Questions/ImageChoice.jsx
+++ src/components/Questions/ImageChoice.jsx
@@ -1,24 +1,145 @@
 import { useState } from "react";
 import React from "react";
+import TextField from "@mui/material/TextField";
 
 const ImageChoice = (props) => {
-  const [questionName, setQuestionName] = useState("");
+  const [question, setQuestion] = useState({
+    questionName: "",
+    questionPic: "",
+    answers: ["", ""],
+    type: "Image Question",
+  });
+  const [enteredQuestionName, setEnteredQuestionName] = useState("");
+  const [enteredQuestionPic, setEnteredQuestionPic] = useState("");
+
+  const handleEnteredAnswers = (e, index) => {
+    if (
+      question.answers[index] !== "" &&
+      question.answers[index] !== e.target.value &&
+      question.answers[question.answers.length - 1] === ""
+    ) {
+      setQuestion((prevState) => {
+        let updatedAnswers = [...prevState.answers];
+        updatedAnswers.splice(index, 1, e.target.value);
+        return {
+          questionName: prevState.questionName,
+          questionPic: prevState.questionPic,
+          answers: updatedAnswers,
+          type: prevState.type,
+        };
+      });
+      return;
+    }
+    if (e.target.value === "") return;
+    if (question.answers[index] === e.target.value) return;
+
+    if (index === 0) {
+      setQuestion((prevState) => {
+        let updatedAnswers = [...prevState.answers];
+        updatedAnswers.splice(index, 1, e.target.value);
+        return {
+          questionName: prevState.questionName,
+          questionPic: prevState.questionPic,
+          answers: updatedAnswers,
+          type: prevState.type,
+        };
+      });
+      return;
+    }
+
+    setQuestion((prevState) => {
+      let updatedAnswers = [...prevState.answers];
+      updatedAnswers.splice(index, 1, e.target.value, "");
+      return {
+        questionName: prevState.questionName,
+        questionPic: prevState.questionPic,
+        answers: updatedAnswers,
+        type: prevState.type,
+      };
+    });
+    return;
+  };
+
+  const handleRemoveAnswer = (index) => {
+    if (index < 2) return;
+    setQuestion((prevState) => {
+      let updatedAnswers = [...prevState.answers];
+      updatedAnswers.splice(index, 1);
+      return {
+        questionName: prevState.questionName,
+        questionPic: prevState.questionPic,
+        answers: updatedAnswers,
+        type: prevState.type,
+      };
+    });
+  };
+
+  const handleQuestionName = (e) => {
+    setEnteredQuestionName(e.target.value);
+    setQuestion((prevState) => {
+      return {
+        questionName: e.target.value,
+        answers: [...prevState.answers],
+        type: prevState.type,
+      };
+    });
+  };
+
+  const handleQuestionPic = (e) => {
+    setEnteredQuestionPic(e.target.value);
+    setQuestion((prevState) => {
+      return {
+        questionName: prevState.questionName,
+        questionPic: e.target.value,
+        answers: [...prevState.answers],
+        type: prevState.type,
+      };
+    });
+  };
+
+  const handleSubmitQuestion = () => {
+    props.onSubmitQuestion(question);
+  };
+
   return (
     <div>
-      <h4>Question Type: Side by side Images</h4>
-      <label>Please enter your question here:</label>
-      <input
-        type="text"
-        value={questionName}
-        onChange={(e) => setQuestionName(e.target.value)}
-        placeholder="Please enter your question here:"
-      />
-      <br></br>
-      <input type="text" placeholder="Enter image url here:" />
-      <br></br>
-      <input type="text" placeholder="Enter image url here:" />
+      <div>
+        <h4>Question Type: Image Question</h4>
+        <label>Please enter your question here:</label>
+        <TextField
+          variant="filled"
+          value={enteredQuestionName}
+          onChange={(e) => handleQuestionName(e)}
+          label="Question"
+        />
+        <br />
+        <label>Please enter your question picture:</label>
+        <TextField
+          variant="filled"
+          value={enteredQuestionPic}
+          onChange={(e) => handleQuestionPic(e)}
+          label="Picture"
+        />
+        <br />
+      </div>
+      {question.answers.map((answer, index) => {
+        return (
+          <div>
+            <TextField
+              variant="filled"
+              label="Answer"
+              onBlur={(e) => handleEnteredAnswers(e, index)}
+            />
+            <input
+              type="button"
+              value="Remove"
+              onClick={() => handleRemoveAnswer(index)}
+            />
+          </div>
+        );
+      })}
       <br />
-      <button>Submit Question</button>
+      <button onClick={handleSubmitQuestion}>Submit Question</button>
     </div>
   );
 };
--- src/pages/NewPoll.jsx
+++ src/pages/NewPoll.jsx
@@ -6,6 +6,7 @@ import MultipleChoice from "../components/Questions/MultipleChoice";
 import ImageChoice from "../components/Questions/ImageChoice";
 import Range from "../components/Questions/Range";
 import TextField from "@mui/material/TextField";
+import { Button } from "react-bootstrap";
 
 const NewPoll = (props) => {
   const [pollName, setPollName] = useState("");
@@ -14,26 +15,101 @@ const NewPoll = (props) => {
     questions: [
       {
         questionName: "",
+        questionPic: "",
         answers: [],
+        type: "",
       },
     ],
+  });
+
+  const [finishedPoll, setFinishedPoll] = useState({
+    pollName: "",
+    questions: [],
   });
 
   const handleFinishedQuestions = (question) => {
     setQuestionToDisplay("");
     setFinishedQuestions((prevState) => {
+      const updatedQuestions = [
+        ...prevState.questions,
+        {
+          questionName: question.questionName,
+          questionPic: question.questionPic,
+          answers: question.answers,
+          type: question.type,
+        },
+      ];
+      console.log(updatedQuestions);
+
       return {
-        questions: [
-          ...prevState.questions,
-          {
-            questionName: question.questionName,
-            answers: question.answers,
-          },
-        ],
+        questions: updatedQuestions,
       };
     });
-    console.log(finishedQuestions);
   };
+
+  async function handleSubmitPoll() {
+    let updatedPoll;
+    setFinishedPoll((prevState) => {
+      let finalQuestions = finishedQuestions.questions;
+      finalQuestions.shift(0);
+      updatedPoll = {
+        pollName: pollName,
+        questions: finalQuestions,
+      };
+      console.log(updatedPoll);
+      return {
+        pollName: updatedPoll.pollName,
+        questions: updatedPoll.questions,
+      };
+    });
+
+    const dataToServer = {
+      pollName: pollName,
+    };
+
+    const accessToken = localStorage.getItem("UserAccessToken");
+
+    const auth = "Bearer " + accessToken;
+
+    const response = await fetch("http://10.10.248.124:8000/poll/create", {
+      method: "POST",
+      body: JSON.stringify(dataToServer),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: auth,
+      },
+    });
+
+    let pollId;
+    const data = await response.json();
+    console.log(response);
+    console.log(data);
+    if (response.ok) {
+      pollId = data._id;
+    }
+    console.log(pollId);
+
+    updatedPoll.questions.forEach((question) => {
+      console.log(question);
+      const data = {
+        pollQuestion: question.questionName,
+        pollQuestionType: question.type,
+        pollQuestionImage: question.questionPic,
+        choices: question.answers,
+        pollId: pollId,
+      };
+      fetch("http://10.10.248.124:8000/poll_question/create", {
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: auth,
+        },
+      })
+        .then((response) => response.json())
+        .then((data) => console.log(data));
+    });
+  }
 
   return (
     <div>
@@ -67,7 +143,7 @@ const NewPoll = (props) => {
               Multiple answers-one choice
             </Dropdown.Item>
             <Dropdown.Item onClick={() => setQuestionToDisplay("image")}>
-              Side by side images
+              Image Question
             </Dropdown.Item>
             <Dropdown.Item onClick={() => setQuestionToDisplay("range")}>
               Select a value from Range
@@ -77,10 +153,12 @@ const NewPoll = (props) => {
         {questionToDisplay === "multiple" && (
           <MultipleChoice onSubmitQuestion={handleFinishedQuestions} />
         )}
-        {questionToDisplay === "image" && <ImageChoice />}
+        {questionToDisplay === "image" && (
+          <ImageChoice onSubmitQuestion={handleFinishedQuestions} />
+        )}
         {questionToDisplay === "range" && <Range />}
       </div>
-
+      <Button onClick={handleSubmitPoll}>Submit Poll</Button>
       <StickyFooter />
     </div>
   );
