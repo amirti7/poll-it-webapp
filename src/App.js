909833866788aa5eda5fb311a8039dd591cfb7f9
Lee wolf <leewolf833@gmail.com>
2022-06-05 14:09:15 +0300

    Added refresh token context

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ import NewPoll from "./pages/NewPoll";
 import UserProfile from "./pages/UserProfile";
 import PrePoll from "./pages/PrePoll";
 import ConfirmPayment from "./pages/ConfirmPayment";
+import { RefreshTokenContext } from "./store/RefreshTokenContext";
 
 // get data from firestore
 initializeApp(config.firebaseConfig);
@@ -30,34 +31,73 @@ const AuthenticatedComponent = ({ children }) => {
   return <LoginPage />;
 };
 
+async function newRefreshToken() {
+  console.log("New Refresh Token Function")
+  let newData, resData;
+
+  const dataToServer = {
+    refreshToken: localStorage.getItem("UserRefreshToken"),
+  };
+
+  const access = localStorage.getItem("UserAccessToken");
+
+  const auth = "Bearer " + access;
+
+  const json = JSON.stringify(dataToServer);
+
+  const res = await fetch("https://poll-it.cs.colman.ac.il/auth/refreshToken", {
+    method: "POST",
+    body: json,
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: auth,
+    },
+  });
+  resData = res;
+  newData = await res.json();
+
+  if (resData.ok) {
+    console.log("Update Tokens")
+    localStorage.setItem("UserRefreshToken", newData.refreshToken);
+    localStorage.setItem("UserAccessToken", newData.accessToken);
+    return (resData)
+  } else {
+    console.log("New Token Error")
+  }
+
+}
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/about_us" element={<AboutUs />} />
-          <Route path="/signup" element={<SignUp />} />
-          <Route path="/NewPoll" element={<NewPoll />} />
-          <Route path="/ConfirmPayment" element={<ConfirmPayment />} />
-          <Route
-            path="/prePoll"
-            element={
-              <AuthenticatedComponent>
-                <PrePoll />
-              </AuthenticatedComponent>
-            }
-          />
-          <Route
-            path="/userProfile"
-            element={
-              <AuthenticatedComponent>
-                <UserProfile />
-              </AuthenticatedComponent>
-            }
-          />
-        </Routes>
+       <RefreshTokenContext.Provider value={newRefreshToken}>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/about_us" element={<AboutUs />} />
+            <Route path="/signup" element={<SignUp />} />
+            <Route path="/NewPoll" element={<NewPoll />} />
+            <Route path="/ConfirmPayment" element={<ConfirmPayment />} />
+            
+            <Route
+              path="/prePoll"
+              element={
+                <AuthenticatedComponent>
+                  <PrePoll />
+                </AuthenticatedComponent>
+              }
+            />
+            <Route
+              path="/userProfile"
+              element={
+                <AuthenticatedComponent>
+                  <UserProfile />
+                </AuthenticatedComponent>
+              }
+            />
+          </Routes>
+        </RefreshTokenContext.Provider>
       </BrowserRouter>
     </div>
   );
